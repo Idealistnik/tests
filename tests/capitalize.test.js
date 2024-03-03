e11bf473f3c61c7294ead8ecf98eb53c02a588ff
Pavlov Nikita <nikitapavlov2015@gmail.com>
2024-03-03 22:39:10 +0300

    fisrt test

---
+++ tests/capitalize.test.js
@@ -0,0 +1,10 @@
+import capitalize from '../src/capitalize.js';
+
+if (capitalize('hello') !== 'Hello') {
+	throw new Error('Функция работает не верно');
+}
+if (capitalize('') !== '') {
+	throw new Error('Функция работает не верно');
+};
+
+console.log('Все тесты пройдены успешно!');
