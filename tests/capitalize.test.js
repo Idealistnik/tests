ad53045470eb4ecd04f386d6dfeb1fb45f042e82
Pavlov Nikita <nikitapavlov2015@gmail.com>
2024-03-04 16:04:45 +0300

    add assert

--- tests/capitalize.test.js
+++ tests/capitalize.test.js
@@ -1,10 +1,14 @@
 import capitalize from '../src/capitalize.js';
+import { strict as assert } from 'node:assert';
 
-if (capitalize('hello') !== 'Hello') {
-	throw new Error('Функция работает не верно');
-}
-if (capitalize('') !== '') {
-	throw new Error('Функция работает не верно');
-};
+// if (capitalize('hello') !== 'Hello') {
+// 	throw new Error('Функция работает не верно');
+// }
+assert.strictEqual(capitalize('hello'), 'Hello')
+assert.strictEqual(capitalize(''), '');
+
+// if (capitalize('') !== '') {
+// 	throw new Error('Функция работает не верно');
+// };
 
 console.log('Все тесты пройдены успешно!');
